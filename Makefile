233194d2da0019c8e73fecf7257d73a468764d90
Guenter Bartsch <guenter@zamia.org>
2016-09-20 23:53:04 +0200

    first steps towards nlp "unit" tests

--- Makefile
+++ Makefile
@@ -6,9 +6,9 @@ prolog:
 	./gen_weather_data.py
 	./prolog_compile.py data/src/common-sense.pl
 	./prolog_compile.py data/dst/weather-dynamic.pl
-	./prolog_compile.py -t data/dst/weather.ts -s data/dst/weather.sem data/src/weather.pl 
-	./prolog_compile.py -t data/dst/greetings.ts -s data/dst/greetings.sem data/src/greetings.pl 
-	./prolog_compile.py -t data/dst/radio.ts -s data/dst/radio.sem data/src/radio.pl 
+	./prolog_compile.py -t data/dst/weather.ts   -s data/dst/weather.sem   -T data/dst/weather.test   data/src/weather.pl 
+	./prolog_compile.py -t data/dst/greetings.ts -s data/dst/greetings.sem -T data/dst/greetings.test data/src/greetings.pl 
+	./prolog_compile.py -t data/dst/radio.ts     -s data/dst/radio.sem     -T data/dst/radio.test     data/src/radio.pl 
 
 nlp_train:
 	./nlp_train_keras.py
--- data/src/radio.pl
+++ data/src/radio.pl
@@ -1,6 +1,20 @@
 % prolog
 
-%! module radio-nlp
+%! module radio
+
+%
+% context setup
+%
+
+context(channel, b5aktuell).
+
+%
+% natural language part
+%
+
+nlp_macro('HI',        map(w(''         )),
+                       map(w('Hallo' )),
+                       map(w('Hi' ))).
 
 nlp_macro('ADDRESSEE', map(w(''         )),
                        map(w('Computer' )),
@@ -9,28 +23,38 @@ nlp_macro('ADDRESSEE', map(w(''         )),
 nlp_macro('PLEASE'   , map(w(''         )),
                        map(w('bitte'    ))).
 
-nlp_macro('VERB',      map(w('schalte'   ), v('ein'), p('turn_on(W)')),
-                       map(w('mach '     ), v('an'),  p('turn_on(W)')),
-                       map(w('mach '     ), v('aus'), p('turn_off(W)')),
-                       map(w('schalte '  ), v('aus'), p('turn_off(W)'))).
+nlp_macro('MAL'      , map(w(''         )),
+                       map(w('mal'      ))).
 
-nlp_macro('WHAT',      map(w('Musik'           ), p('W is music')),
-                       map(w('das Radio '      ), p('W is radio')),
-                       map(w('B5 aktuell'      ), p('W is b5aktuell')),
-                       map(w('Deutschlandfunk' ), p('W is dlf')),
-                       map(w('SWR Info'        ), p('W is swrinfo')),
-                       map(w('SWR 3'           ), p('W is swr3')),
-                       map(w('Aktuelles'       ), p('W is news')),
-                       map(w('Newstalk'        ), p('W is newstalk')),
-                       map(w('Power Hit Radio' ), p('W is powerhitradio')),
-                       map(w('RTL'             ), p('W is rtl')),
-                       map(w('NEW ROCK'        ), p('W is newrock')),
-                       map(w('ROCK'            ), p('W is rock')),
-                       map(w('POP'             ), p('W is pop')),
-                       map(w('DANCE'           ), p('W is dance')),
-                       map(w('Tech News'       ), p('W is technews')),
-                       map(w('Workout'         ), p('W is workout'))).
+nlp_macro('VERB',      map(w('schalte'   ), v('ein'), p('set_context(channel, C), action(media, tune, C).')),
+                       map(w('mach '     ), v('an'),  p('set_context(channel, C), action(media, tune, C).')),
+                       map(w('mach '     ), v('aus'), p('action(media, off).')),
+                       map(w('schalte '  ), v('aus'), p('action(media, off).'))).
 
-nlp_gen('@ADDRESSEE:w @VERB:w @PLEASE:w @WHAT:w @VERB:v',
+nlp_macro('WHAT',      map(w('Musik'           ), p('C is music')),
+                       map(w('das Radio '      ), p('context(channel, C)')),
+                       map(w('B5 aktuell'      ), p('C is b5aktuell')),
+                       map(w('Deutschlandfunk' ), p('C is dlf')),
+                       map(w('SWR Info'        ), p('C is swrinfo')),
+                       map(w('SWR 3'           ), p('C is swr3')),
+                       map(w('Aktuelles'       ), p('C is news')),
+                       map(w('Newstalk'        ), p('C is newstalk')),
+                       map(w('Power Hit Radio' ), p('C is powerhitradio')),
+                       map(w('RTL'             ), p('C is rtl')),
+                       map(w('NEW ROCK'        ), p('C is newrock')),
+                       map(w('ROCK'            ), p('C is rock')),
+                       map(w('POP'             ), p('C is pop')),
+                       map(w('DANCE'           ), p('C is dance')),
+                       map(w('Tech News'       ), p('C is technews')),
+                       map(w('Workout'         ), p('C is workout'))).
+
+nlp_gen('@HI:w @ADDRESSEE:w @VERB:w @PLEASE:w @MAL:w @WHAT:w @VERB:v',
         '@WHAT:p; @VERB:p').
+
+nlp_test(ivr(in('computer schalte bitte new rock ein'),
+             action(media, tune, newrock)),
+         ivr(in('hal mach das radio aus'),
+             action(media, off)),
+         ivr(in('schalte bitte das radio ein'),
+             action(media, tune, newrock))). 
 

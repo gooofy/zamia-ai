d4d24a98d12dc110e5c66715cf935c260b27ac78
Guenter Bartsch <guenter@zamia.org>
2016-11-17 16:33:34 +0100

    export nnet3 models, switch to xz compression

--- speech_dist.sh
+++ speech_dist.sh
@@ -8,59 +8,65 @@ mkdir $DISTDIR
 datum=`date +%Y%m%d`
 
 #
-# cont sphinx model de
+# kaldi nnet3 models de
 #
 
-AMNAME="cmusphinx-cont-voxforge-de-r$datum"
+AMNAME="kaldi-nnet3-voxforge-de-r$datum"
 
 mkdir "$DISTDIR/$AMNAME"
-mkdir "$DISTDIR/$AMNAME/model_parameters"
 
-cp -r data/dst/speech/de/cmusphinx_cont/model_parameters/voxforge.cd_cont_6000 "$DISTDIR/$AMNAME/model_parameters"
-cp -r data/dst/speech/de/cmusphinx_cont/etc "$DISTDIR/$AMNAME"
-cp data/dst/speech/de/cmusphinx_cont/voxforge.html "$DISTDIR/$AMNAME"
+function export_kaldi_nnet3 {
+
+    EXPNAME=$1
+    GRAPHNAME=$2
+
+    mkdir "$DISTDIR/$AMNAME/$EXPNAME"
+
+    cp data/dst/speech/de/kaldi/exp/nnet3/$EXPNAME/final.mdl   $DISTDIR/$AMNAME/$EXPNAME/
+    cp data/dst/speech/de/kaldi/exp/nnet3/$EXPNAME/cmvn_opts   $DISTDIR/$AMNAME/$EXPNAME/          2>/dev/null 
+
+    cp data/dst/speech/de/kaldi/exp/nnet3/$GRAPHNAME/HCLG.fst  $DISTDIR/$AMNAME/$EXPNAME/
+    cp data/dst/speech/de/kaldi/exp/nnet3/$GRAPHNAME/words.txt $DISTDIR/$AMNAME/$EXPNAME/
+    cp data/dst/speech/de/kaldi/exp/nnet3/$GRAPHNAME/num_pdfs  $DISTDIR/$AMNAME/$EXPNAME/
+
+}
+
+export_kaldi_nnet3 nnet_tdnn_a  nnet_tdnn_a/graph
+export_kaldi_nnet3 lstm_ld5     lstm_ld5/graph
+
+mkdir "$DISTDIR/$AMNAME/extractor"
+
+cp data/dst/speech/de/kaldi/exp/nnet3/extractor/final.mat            "$DISTDIR/$AMNAME/extractor/"
+cp data/dst/speech/de/kaldi/exp/nnet3/extractor/global_cmvn.stats    "$DISTDIR/$AMNAME/extractor/"
+cp data/dst/speech/de/kaldi/exp/nnet3/extractor/final.dubm           "$DISTDIR/$AMNAME/extractor/"
+cp data/dst/speech/de/kaldi/exp/nnet3/extractor/final.ie             "$DISTDIR/$AMNAME/extractor/"
+cp data/dst/speech/de/kaldi/exp/nnet3/extractor/splice_opts          "$DISTDIR/$AMNAME/extractor/"
+cp data/dst/speech/de/kaldi/exp/nnet3/ivectors_test/conf/splice.conf "$DISTDIR/$AMNAME/extractor/"
+
+cp data/dst/speech/de/kaldi/RESULTS.txt $DISTDIR/$AMNAME/
 cp README.md "$DISTDIR/$AMNAME"
 cp COPYING   "$DISTDIR/$AMNAME"
 cp AUTHORS   "$DISTDIR/$AMNAME"
 
+mkdir "$DISTDIR/$AMNAME/conf"
+cp data/src/speech/kaldi-mfcc.conf        $DISTDIR/$AMNAME/conf/mfcc.conf 
+cp data/src/speech/kaldi-mfcc-hires.conf  $DISTDIR/$AMNAME/conf/mfcc-hires.conf  
+cp data/src/speech/kaldi-online-cmvn.conf $DISTDIR/$AMNAME/conf/online_cmvn.conf
+
 pushd $DISTDIR
-tar cfvz "$AMNAME.tgz" $AMNAME
+tar cfv "$AMNAME.tar" $AMNAME
+xz -v -8 -T 12 "$AMNAME.tar"
 popd
 
 rm -r "$DISTDIR/$AMNAME"
 
 #
-# ptm sphinx model de
+# kaldi gmm models de
 #
 
-AMNAME="cmusphinx-ptm-voxforge-de-r$datum"
+AMNAME="kaldi-gmm-voxforge-de-r$datum"
 
 mkdir "$DISTDIR/$AMNAME"
-mkdir "$DISTDIR/$AMNAME/model_parameters"
-
-cp -r data/dst/speech/de/cmusphinx_ptm/model_parameters/voxforge.cd_ptm_5000 "$DISTDIR/$AMNAME/model_parameters"
-cp -r data/dst/speech/de/cmusphinx_ptm/etc "$DISTDIR/$AMNAME"
-cp data/dst/speech/de/cmusphinx_ptm/voxforge.html "$DISTDIR/$AMNAME"
-cp README.md "$DISTDIR/$AMNAME"
-cp COPYING   "$DISTDIR/$AMNAME"
-cp AUTHORS   "$DISTDIR/$AMNAME"
-
-pushd $DISTDIR
-tar cfvz "$AMNAME.tgz" $AMNAME
-popd
-
-rm -r "$DISTDIR/$AMNAME"
-
-#
-# kaldi models de
-#
-
-AMNAME="kaldi-voxforge-de-r$datum"
-
-mkdir "$DISTDIR/$AMNAME"
-
-#     EXPNAME=tri2b_mmi_b0.05
-#     GRAPHNAME=tri2b_denlats/dengraph
 
 function export_kaldi_model {
 
@@ -69,8 +75,8 @@ function export_kaldi_model {
 
     mkdir "$DISTDIR/$AMNAME/$EXPNAME"
 
-    cp data/dst/speech/de/kaldi/exp/$EXPNAME/final.mdl   $DISTDIR/$AMNAME/$EXPNAME/model.mdl
-    cp data/dst/speech/de/kaldi/exp/$EXPNAME/final.mat   $DISTDIR/$AMNAME/$EXPNAME/model.mat
+    cp data/dst/speech/de/kaldi/exp/$EXPNAME/final.mdl   $DISTDIR/$AMNAME/$EXPNAME/
+    cp data/dst/speech/de/kaldi/exp/$EXPNAME/final.mat   $DISTDIR/$AMNAME/$EXPNAME/
     cp data/dst/speech/de/kaldi/exp/$EXPNAME/splice_opts $DISTDIR/$AMNAME/$EXPNAME/          2>/dev/null
     cp data/dst/speech/de/kaldi/exp/$EXPNAME/cmvn_opts   $DISTDIR/$AMNAME/$EXPNAME/          2>/dev/null 
     cp data/dst/speech/de/kaldi/exp/$EXPNAME/delta_opts  $DISTDIR/$AMNAME/$EXPNAME/          2>/dev/null 
@@ -101,7 +107,54 @@ cp data/src/speech/kaldi-mfcc-hires.conf  $DISTDIR/$AMNAME/conf/mfcc-hires.conf
 cp data/src/speech/kaldi-online-cmvn.conf $DISTDIR/$AMNAME/conf/online_cmvn.conf
 
 pushd $DISTDIR
-tar cfvz "$AMNAME.tgz" $AMNAME
+tar cfv "$AMNAME.tar" $AMNAME
+xz -v -8 -T 12 "$AMNAME.tar"
+popd
+
+rm -r "$DISTDIR/$AMNAME"
+
+#
+# cont sphinx model de
+#
+
+AMNAME="cmusphinx-cont-voxforge-de-r$datum"
+
+mkdir "$DISTDIR/$AMNAME"
+mkdir "$DISTDIR/$AMNAME/model_parameters"
+
+cp -r data/dst/speech/de/cmusphinx_cont/model_parameters/voxforge.cd_cont_6000 "$DISTDIR/$AMNAME/model_parameters"
+cp -r data/dst/speech/de/cmusphinx_cont/etc "$DISTDIR/$AMNAME"
+cp data/dst/speech/de/cmusphinx_cont/voxforge.html "$DISTDIR/$AMNAME"
+cp README.md "$DISTDIR/$AMNAME"
+cp COPYING   "$DISTDIR/$AMNAME"
+cp AUTHORS   "$DISTDIR/$AMNAME"
+
+pushd $DISTDIR
+tar cfv "$AMNAME.tar" $AMNAME
+xz -v -8 -T 12 "$AMNAME.tar"
+popd
+
+rm -r "$DISTDIR/$AMNAME"
+
+#
+# ptm sphinx model de
+#
+
+AMNAME="cmusphinx-ptm-voxforge-de-r$datum"
+
+mkdir "$DISTDIR/$AMNAME"
+mkdir "$DISTDIR/$AMNAME/model_parameters"
+
+cp -r data/dst/speech/de/cmusphinx_ptm/model_parameters/voxforge.cd_ptm_5000 "$DISTDIR/$AMNAME/model_parameters"
+cp -r data/dst/speech/de/cmusphinx_ptm/etc "$DISTDIR/$AMNAME"
+cp data/dst/speech/de/cmusphinx_ptm/voxforge.html "$DISTDIR/$AMNAME"
+cp README.md "$DISTDIR/$AMNAME"
+cp COPYING   "$DISTDIR/$AMNAME"
+cp AUTHORS   "$DISTDIR/$AMNAME"
+
+pushd $DISTDIR
+tar cfv "$AMNAME.tar" $AMNAME
+xz -v -8 -T 12 "$AMNAME.tar"
 popd
 
 rm -r "$DISTDIR/$AMNAME"

71e5062d3a528ef4304095c59486b944d96f765c
Guenter Bartsch <guenter@zamia.org>
2017-03-15 23:37:33 +0100

    some more work on the smalltalk module, remember partner name and gender

---
+++ modules/smalltalk/cars.pl
@@ -0,0 +1,6 @@
+% prolog
+
+nlp_gen (de, '(HAL,|Computer,|) (ohje, mein | ich fuhr mit dem | ich mache mir sorgen um mein | ich brauche ein neues | cool, ein) (Auto|Wagen|Kraftfahrzeug)',
+             'Was für einen Wagen fährst Du?', 'Welche Marke?', 'Hast Du auch ein Fahrrad?').
+
+
---
+++ modules/smalltalk/smalltalk.pl
@@ -0,0 +1,12 @@
+% prolog
+
+nlp_gen(de, '(HAL,|Computer,|) Ist die Hände zu halten eine symbolische Geste für Menschen?',
+            'Oh, ja!',
+            'Das ist ein schöner Gedanke.').
+
+nlp_gen (de, '(HAL,|Computer,|) (man|) (Du bist ein|am|alles ist am|so ein) Arsch',
+             'Warum benutzt Du solche Worte?', 'Das sagt man doch nicht!').
+
+nlp_gen (de, '(HAL,|Computer,|) (das ist ja | das ist | zu | du bist | ich glaube du bist | du bist ganz schön) dumm',
+             'dumm im sinne von nicht intelligent?', 'sprechen wir wirklich von mir?', 'denkst du?').
+

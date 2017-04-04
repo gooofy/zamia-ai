21c7333535d5ad53b291b883876a5cbae92fc92f
Guenter Bartsch <guenter@zamia.org>
2017-04-04 21:24:11 +0200

    radio english translation added

--- modules/radio/radio.pl
+++ modules/radio/radio.pl
@@ -12,7 +12,7 @@ init ('radio') :-
 % test setup and context
 %
 
-test_setup('weather') :-
+test_setup('radio') :-
     rdf (aiu:self, ai:favChannel, CHANNEL),
     context_set (channel, CHANNEL),
     eoa.
@@ -37,13 +37,30 @@ media_tune (C) :-
 % natural language part
 %
 
-nlp_macro('VERB', W, V, P) :- W is 'schalte'   , V is 'ein', P is 'media_tune(C)'.
-nlp_macro('VERB', W, V, P) :- W is 'mach '     , V is 'an',  P is 'media_tune(C)'.
-nlp_macro('VERB', W, V, P) :- W is 'mach '     , V is 'aus', P is 'action(media, off);eoa'.
-nlp_macro('VERB', W, V, P) :- W is 'schalte '  , V is 'aus', P is 'action(media, off);eoa'.
+nlp_macro('VERB_EN', W, P) :- W is 'switch to' , P is 'media_tune(C)'.
+nlp_macro('VERB_EN', W, P) :- W is 'switch on' , P is 'media_tune(C)'.
+nlp_macro('VERB_EN', W, P) :- W is 'tune to'   , P is 'media_tune(C)'.
+nlp_macro('VERB_EN', W, P) :- W is 'turn on'   , P is 'media_tune(C)'.
+nlp_macro('VERB_EN', W, P) :- W is 'tune into' , P is 'media_tune(C)'.
+nlp_macro('VERB_EN', W, P) :- W is 'switch off', P is 'action(media, off);eoa'.
+nlp_macro('VERB_EN', W, P) :- W is 'turn off'  , P is 'action(media, off);eoa'.
 
-nlp_macro('STATION', W, P) :- W is 'das Radio' , P is 'context_get(channel, C)'.
-nlp_macro('STATION', W, P) :-
+nlp_macro('VERB_DE', W, V, P) :- W is 'schalte'   , V is 'ein', P is 'media_tune(C)'.
+nlp_macro('VERB_DE', W, V, P) :- W is 'mach '     , V is 'an',  P is 'media_tune(C)'.
+nlp_macro('VERB_DE', W, V, P) :- W is 'mach '     , V is 'aus', P is 'action(media, off);eoa'.
+nlp_macro('VERB_DE', W, V, P) :- W is 'schalte '  , V is 'aus', P is 'action(media, off);eoa'.
+
+nlp_macro('STATION_EN', W, P) :- W is 'the radio' , P is 'context_get(channel, C)'.
+nlp_macro('STATION_EN', W, P) :-
+    rdf (distinct,
+         STATION, ai:MediaSlot, SLOT,
+         STATION, rdfs:label, LABEL,
+         filter(lang(LABEL) = 'en')),
+    W is LABEL,
+    P is format_str('C is "%s"', STATION).
+
+nlp_macro('STATION_DE', W, P) :- W is 'das Radio' , P is 'context_get(channel, C)'.
+nlp_macro('STATION_DE', W, P) :-
     rdf (distinct,
          STATION, ai:MediaSlot, SLOT,
          STATION, rdfs:label, LABEL,
@@ -51,28 +68,45 @@ nlp_macro('STATION', W, P) :-
     W is LABEL,
     P is format_str('C is "%s"', STATION).
 
+nlp_gen(en, 
+        '@SELF_ADDRESS_EN:LABEL (please|) @VERB_EN:W @STATION_EN:W',
+        @STATION_EN:P, @VERB_EN:P).
 nlp_gen(de, 
-        '(Hallo|Hi|) (HAL|Computer|) @VERB:W (bitte|) (mal|) @STATION:W @VERB:V',
-        @STATION:P, @VERB:P).
+        '@SELF_ADDRESS_DE:LABEL @VERB_DE:W (bitte|) (mal|) @STATION_DE:W @VERB_DE:V',
+        @STATION_DE:P, @VERB_DE:P).
 
+nlp_test(en,
+         ivr(in('computer, please switch on the radio'),
+             action(media, tune, 9, 1))).
 nlp_test(de,
-         ivr(in('HAL, schalte bitte das Radio ein'),
+         ivr(in('computer, schalte bitte das Radio ein'),
              action(media, tune, 9, 1))).
 
+nlp_test(en,
+         ivr(in('computer please tune into new rock'),
+             action(media, tune, 3, []))).
 nlp_test(de,
          ivr(in('computer schalte bitte new rock ein'),
              action(media, tune, 3, []))).
 
-
+nlp_test(en,
+         ivr(in('turn off the radio'),
+             action(media, off))).
 nlp_test(de,
          ivr(in('mach das radio aus'),
              action(media, off))).
 
-
+nlp_test(en,
+         ivr(in('computer please switch on new rock'),
+             action(media, tune, 3, [])),
+         ivr(in('switch off the radio'),
+             action(media, off)),
+         ivr(in('please turn on the radio'),
+             action(media, tune, 3, []))). 
 nlp_test(de,
          ivr(in('computer schalte bitte new rock ein'),
              action(media, tune, 3, [])),
-         ivr(in('hal mach das radio aus'),
+         ivr(in('mach das radio aus'),
              action(media, off)),
          ivr(in('schalte bitte das radio ein'),
              action(media, tune, 3, []))). 

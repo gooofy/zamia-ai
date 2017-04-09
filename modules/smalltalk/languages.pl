bc1a0c69da14c229d9a926dbe1b146ea992986c9
Guenter Bartsch <guenter@zamia.org>
2017-04-10 00:43:25 +0200

    language topic added

--- modules/smalltalk/languages.pl
+++ modules/smalltalk/languages.pl
@@ -1,5 +1,10 @@
 %prolog
 
+answer(topic, en) :-
+    context_score(topic, language, 100, SCORE), say_eoa(en, 'We were talking about languages.', SCORE).
+answer(topic, de) :-
+    context_score(topic, language, 100, SCORE), say_eoa(de, 'Wir hatten das Thema Sprachen.', SCORE).
+
 % <?xml version='1.0' encoding='utf8'?>',
 % <ns0:template',
 %  xmlns:ns0="http://alicebot.org/2001/AIML-1.0.1"',

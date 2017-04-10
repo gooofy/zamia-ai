5bb5e5f8e5837024745c3a55b575c8b774ad686e
Guenter Bartsch <guenter@zamia.org>
2017-04-10 11:18:52 +0200

    add non-empty variant

--- modules/base/base.pl
+++ modules/base/base.pl
@@ -4,7 +4,13 @@
 nlp_macro('SELF_ADDRESS_EN', LABEL) :- rdf (aiu:self, ai:forename, LABEL, filter(lang(LABEL) = 'en')).
 nlp_macro('SELF_ADDRESS_EN', LABEL) :- LABEL is ''.
 
+% NE: not empty
+nlp_macro('SELF_ADDRESS_EN_NE', LABEL) :- rdf (aiu:self, ai:forename, LABEL, filter(lang(LABEL) = 'en')).
+
 % nlp_macro('SELF_ADDRESS_DE', LABEL) :- rdf (aiu:self, rdfs:label, LABEL, filter(lang(LABEL) = 'de')).
 nlp_macro('SELF_ADDRESS_DE', LABEL) :- rdf (aiu:self, ai:forename, LABEL, filter(lang(LABEL) = 'de')).
 nlp_macro('SELF_ADDRESS_DE', LABEL) :- LABEL is ''.
+
+% NE: not empty
+nlp_macro('SELF_ADDRESS_DE_NE', LABEL) :- rdf (aiu:self, ai:forename, LABEL, filter(lang(LABEL) = 'de')).
 

2c62a8ebe62ee2a44969a0ca960043bca1cbc2c6
Guenter Bartsch <guenter@zamia.org>
2017-04-19 21:45:03 +0200

    move knowledge base stuff to central location in base module

--- modules/politics/__init__.py
+++ modules/politics/__init__.py
@@ -10,24 +10,8 @@ PL_SOURCES = [
              ]
 
 RDF_ALIASES = {
-                u'wde:PresidentOfGermany'         : u'http://www.wikidata.org/entity/Q25223',
-                u'wde:FederalChancellorOfGermany' : u'http://www.wikidata.org/entity/Q4970706',
               }
 
-KB_SOURCES = [
-
-              # people
-
-              (
-                [ 
-                  ('wdpd:PositionHeld', 'wde:FederalChancellorOfGermany'),
-                  ('wdpd:PositionHeld', 'wde:PresidentOfGermany'),
-                ],
-                [
-                  ['wdpd:PlaceOfBirth'], 
-                  ['wdp:PositionHeld','*']
-                ]
-              ),
-
-            ]
+KB_SOURCES  = [
+              ]
 

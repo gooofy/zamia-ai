d63c4bb28157eac3918b195f1cfa90c4a0420c84
Guenter Bartsch <guenter@zamia.org>
2017-04-24 08:23:38 +0200

    NER for the tech module

--- modules/tech/tech.pl
+++ modules/tech/tech.pl
@@ -1,5 +1,62 @@
 % prolog
 
+is_computer_scientist(PERSON) :- rdf (PERSON, wdpd:Occupation, wde:ComputerScientist).
+
+%
+% named entity recognition (NER) stuff: extra points for computer scientists, programming language and home computer NER
+% FIXME: NER for operating systems, ...
+%
+
+ner_score (person, PERSON, 200) :- is_computer_scientist(PERSON).
+
+ner_score (programming_language, PROGRAMMING_LANGUAGE, 100). % FIXME: we should probably score by popularity or smth
+
+ner_programming_language(LANG, TITLE_TOKENS, PROGRAMMING_LANGUAGE, LABEL) :-
+
+    atom_chars(LANG, LSTR),
+
+    rdf (distinct,
+         PROGRAMMING_LANGUAGE, wdpd:InstanceOf,   wde:ProgrammingLanguage,
+         PROGRAMMING_LANGUAGE, rdfs:label,        LABEL,
+         filter (lang(LABEL) = LSTR)),
+
+    tokenize (LANG, LABEL, LABEL_TOKENS),
+
+    TITLE_TOKENS = LABEL_TOKENS.
+
+ner(LANG, programming_language, TSTART, TEND, PROGRAMMING_LANGUAGE, LABEL, SCORE) :-
+
+    rdf(ai:curin, ai:tokens, TOKENS),
+    list_slice(TSTART, TEND, TOKENS, NAME_TOKENS),
+   
+    ner_programming_language(LANG, NAME_TOKENS, PROGRAMMING_LANGUAGE, LABEL),
+
+    ner_score (programming_language, PROGRAMMING_LANGUAGE, SCORE).
+
+ner_score (home_computer, HOME_COMPUTER, 100). % FIXME: we should probably score by popularity or smth
+
+ner_home_computer(LANG, TITLE_TOKENS, HOME_COMPUTER, LABEL) :-
+
+    atom_chars(LANG, LSTR),
+
+    rdf (distinct,
+         HOME_COMPUTER, wdpd:InstanceOf,   wde:HomeComputer,
+         HOME_COMPUTER, rdfs:label,        LABEL,
+         filter (lang(LABEL) = LSTR)),
+
+    tokenize (LANG, LABEL, LABEL_TOKENS),
+
+    TITLE_TOKENS = LABEL_TOKENS.
+
+ner(LANG, home_computer, TSTART, TEND, HOME_COMPUTER, LABEL, SCORE) :-
+
+    rdf(ai:curin, ai:tokens, TOKENS),
+    list_slice(TSTART, TEND, TOKENS, NAME_TOKENS),
+   
+    ner_home_computer(LANG, NAME_TOKENS, HOME_COMPUTER, LABEL),
+
+    ner_score (home_computer, HOME_COMPUTER, SCORE).
+
 answer(topic, en) :-
     context_score(topic, computers, 100, SCORE), say_eoa(en, 'We were talking about computers and machines.', SCORE).
 answer(topic, de) :-
@@ -21,6 +78,11 @@ answer(topic, de) :-
     context_score(topic, home_computer, 100, SCORE), say_eoa(de, 'Wir hatten über Heimcomputer gesprochen.', SCORE).
 
 answer(topic, en) :-
+    context_score(topic, computer_science, 100, SCORE), say_eoa(en, 'We were talking about computer science.', SCORE).
+answer(topic, de) :-
+    context_score(topic, computer_science, 100, SCORE), say_eoa(de, 'Wir hatten über Informatik gesprochen.', SCORE).
+
+answer(topic, en) :-
     context_score(topic, linux, 100, SCORE), say_eoa(en, 'We were talking about Linux.', SCORE).
 answer(topic, de) :-
     context_score(topic, linux, 100, SCORE), say_eoa(de, 'Wir hatten über Linux gesprochen.', SCORE).
@@ -37,6 +99,43 @@ nlp_test(de,
          ivr(in('magst du linux?'),
              out('Hey, Linux ist mein Betriebssystem, das ist richtig cool.'))).
 
+answer (knownPerson, en, PERSON, LABEL, SCORE) :-
+    context_score (topic, computer_science, 100, SCORE),
+    is_computer_scientist(PERSON),
+    is_male(PERSON),
+    context_push(topic, computer_science),
+    context_push(topic, PERSON),
+    say_eoa(en, 'He is a computer scientist.', SCORE).
+answer (knownPerson, de, PERSON, LABEL, SCORE) :-
+    context_score (topic, computer_science, 100, SCORE),
+    is_computer_scientist(PERSON),
+    is_male(PERSON),
+    context_push(topic, computer_science),
+    context_push(topic, PERSON),
+    say_eoa(de, 'Er ist ein Informatiker.', SCORE).
+
+answer (knownPerson, en, PERSON, LABEL, SCORE) :-
+    context_score (topic, computer_science, 100, SCORE),
+    is_computer_scientist(PERSON),
+    is_female(PERSON),
+    context_push(topic, computer_science),
+    context_push(topic, PERSON),
+    say_eoa(en, 'She is a computer scientist.', SCORE).
+answer (knownPerson, de, PERSON, LABEL, SCORE) :-
+    context_score (topic, computer_science, 100, SCORE),
+    is_computer_scientist(PERSON),
+    is_female(PERSON),
+    context_push(topic, computer_science),
+    context_push(topic, PERSON),
+    say_eoa(de, 'Sie ist eine Informatikerin.', SCORE).
+
+nlp_test(en,
+         ivr(in('Who is Niklaus Wirth?'),
+             out('He is a computer scientist.'))).
+nlp_test(de,
+         ivr(in('wer ist Niklaus Wirth?'),
+             out('Er ist ein Informatiker.'))).
+
 nlp_macro('PROGRAMMING_LANGUAGE_EN', NAME, LABEL) :- 
     rdf(distinct,
         NAME, wdpd:InstanceOf, wde:ProgrammingLanguage,
@@ -48,19 +147,35 @@ nlp_macro('PROGRAMMING_LANGUAGE_DE', NAME, LABEL) :-
         NAME, rdfs:label, LABEL,
         filter(lang(LABEL) = 'de')).
 
+answer (knownProgrammingLanguage, en, PROGRAMMING_LANGUAGE, PROGRAMMING_LANGUAGE_LABEL, SCORE) :-
+    context_push(topic, programming),
+    context_push(topic, PROGRAMMING_LANGUAGE),
+    say_eoa(en, format_str('%s is a programming language.', PROGRAMMING_LANGUAGE_LABEL), SCORE).
+answer (knownProgrammingLanguage, de, PROGRAMMING_LANGUAGE, PROGRAMMING_LANGUAGE_LABEL, SCORE) :-
+    context_push(topic, programming),
+    context_push(topic, PROGRAMMING_LANGUAGE),
+    say_eoa(de, format_str('%s ist eine Programmiersprache.', PROGRAMMING_LANGUAGE_LABEL), SCORE).
+
+answer (knownProgrammingLanguageTokens, en, TSTART, TEND) :-
+    ner(en, programming_language, TSTART, TEND, PROGRAMMING_LANGUAGE, PROGRAMMING_LANGUAGE_LABEL, SCORE),
+    answer (knownProgrammingLanguage, en, PROGRAMMING_LANGUAGE, PROGRAMMING_LANGUAGE_LABEL, SCORE).
+answer (knownProgrammingLanguageTokens, de, TSTART, TEND) :-
+    ner(de, programming_language, TSTART, TEND, PROGRAMMING_LANGUAGE, PROGRAMMING_LANGUAGE_LABEL, SCORE),
+    answer (knownProgrammingLanguage, de, PROGRAMMING_LANGUAGE, PROGRAMMING_LANGUAGE_LABEL, SCORE).
+
 nlp_gen(en, '@SELF_ADDRESS_EN:LABEL (do you know|what is) @PROGRAMMING_LANGUAGE_EN:LABEL?',
-            context_push(topic, programming), context_push(topic, "@PROGRAMMING_LANGUAGE_EN:NAME"), say_eoa(en, 'Yes, that is a programming language')).
+             answer(knownProgrammingLanguageTokens, en, @PROGRAMMING_LANGUAGE_EN:TSTART_LABEL_0, @PROGRAMMING_LANGUAGE_EN:TEND_LABEL_0)). 
 nlp_gen(de, '@SELF_ADDRESS_DE:LABEL (kennst Du|was ist) @PROGRAMMING_LANGUAGE_DE:LABEL?',
-            context_push(topic, programming), context_push(topic, "@PROGRAMMING_LANGUAGE_DE:NAME"), say_eoa(de, 'Ja, das ist eine Programmiersprache')).
+             answer(knownProgrammingLanguageTokens, de, @PROGRAMMING_LANGUAGE_DE:TSTART_LABEL_0, @PROGRAMMING_LANGUAGE_DE:TEND_LABEL_0)). 
 
 nlp_test(en,
          ivr(in('do you know prolog?'),
-             out("Yes that is a programming language")),
+             out("Prolog is a programming language")),
          ivr(in('what was our topic, again?'),
              out("We were talking about prolog."))).
 nlp_test(de,
          ivr(in('kennst du prolog?'),
-             out('Ja, das ist eine Programmiersprache')),
+             out('Prolog ist eine Programmiersprache')),
          ivr(in('Worüber hatten wir gesprochen?'),
              out("Wir hatten über prolog gesprochen."))).
 
@@ -75,19 +190,35 @@ nlp_macro('HOME_COMPUTER_DE', NAME, LABEL) :-
         NAME, rdfs:label, LABEL,
         filter(lang(LABEL) = 'de')).
 
+answer (knownHomeComputer, en, HOME_COMPUTER, HOME_COMPUTER_LABEL, SCORE) :-
+    context_push(topic, programming),
+    context_push(topic, HOME_COMPUTER),
+    say_eoa(en, format_str('The %s is a home computer.', HOME_COMPUTER_LABEL), SCORE).
+answer (knownHomeComputer, de, HOME_COMPUTER, HOME_COMPUTER_LABEL, SCORE) :-
+    context_push(topic, programming),
+    context_push(topic, HOME_COMPUTER),
+    say_eoa(de, format_str('Der %s ist ein Heimcomputer.', HOME_COMPUTER_LABEL), SCORE).
+
+answer (knownHomeComputerTokens, en, TSTART, TEND) :-
+    ner(en, home_computer, TSTART, TEND, HOME_COMPUTER, HOME_COMPUTER_LABEL, SCORE),
+    answer (knownHomeComputer, en, HOME_COMPUTER, HOME_COMPUTER_LABEL, SCORE).
+answer (knownHomeComputerTokens, de, TSTART, TEND) :-
+    ner(de, home_computer, TSTART, TEND, HOME_COMPUTER, HOME_COMPUTER_LABEL, SCORE),
+    answer (knownHomeComputer, de, HOME_COMPUTER, HOME_COMPUTER_LABEL, SCORE).
+
 nlp_gen(en, '@SELF_ADDRESS_EN:LABEL (do you know|what is) @HOME_COMPUTER_EN:LABEL?',
-            context_push(topic, home_computer), context_push(topic, "@HOME_COMPUTER_EN:NAME"), say_eoa(en, 'Yes, that was a home computer')).
+            answer(knownHomeComputerTokens, en, @HOME_COMPUTER_EN:TSTART_LABEL_0, @HOME_COMPUTER_EN:TEND_LABEL_0)). 
 nlp_gen(de, '@SELF_ADDRESS_DE:LABEL (kennst Du|was ist) @HOME_COMPUTER_DE:LABEL?',
-            context_push(topic, home_computer), context_push(topic, "@HOME_COMPUTER_DE:NAME"), say_eoa(de, 'Ja, das ist ein Heimcomputer')).
+            answer(knownHomeComputerTokens, de, @HOME_COMPUTER_DE:TSTART_LABEL_0, @HOME_COMPUTER_DE:TEND_LABEL_0)). 
 
 nlp_test(en,
          ivr(in('do you know commodore 64?'),
-             out("Yes that was a home computer")),
+             out("The Commodore 64 is a home computer")),
          ivr(in('what was our topic, again?'),
              out("We were talking about Commodore 64."))).
 nlp_test(de,
          ivr(in('kennst du sinclair ql?'),
-             out('Ja, das ist ein Heimcomputer')),
+             out('Der Sinclair QL ist ein Heimcomputer')),
          ivr(in('Worüber hatten wir gesprochen?'),
              out("Wir hatten über Sinclair QL gesprochen."))).
 

700645e86c6242b7139aa81ff2025b5bc1368aef
Guenter Bartsch <guenter@zamia.org>
2017-04-24 12:39:36 +0200

    move some questions over to the personality module

--- modules/smalltalk/religion.pl
+++ modules/smalltalk/religion.pl
@@ -5,13 +5,6 @@ answer(topic, en) :-
 answer(topic, de) :-
     context_score(topic, religion, 100, SCORE), say_eoa(de, 'Wir hatten das Thema Glaube und Religion.', SCORE).
 
-nlp_gen (en, "@SELF_ADDRESS_EN:LABEL (oh my|do you believe in|is there a|by) god",
-             context_push(topic, religion), say_eoa(en, "are you a religious person?"), 
-             context_push(topic, religion), say_eoa(en, "do you believe in god")).
-nlp_gen (de, '@SELF_ADDRESS_DE:LABEL (oh mein|glaubst du an|gibt es einen|bei) gott',
-             context_push(topic, religion), say_eoa(de, 'Bist Du ein gläubiger Mensch?'), 
-             context_push(topic, religion), say_eoa(de, 'glaubst du denn an gott?')).
-
 nlp_gen (en, "@SELF_ADDRESS_EN:LABEL (are you|am I) (jesus|god)",
              "Ich bin ein Berliner.").
 nlp_gen (de, '@SELF_ADDRESS_DE:LABEL (bist du|bin ich) (jesus|gott)',

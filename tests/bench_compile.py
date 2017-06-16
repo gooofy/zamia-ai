2fe4e366692d0af324c6793956f6dea0412ff729
Guenter Bartsch <guenter@zamia.org>
2017-06-16 14:31:39 +0200

    compile benchmark added

---
+++ tests/bench_compile.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+#
+# Copyright 2017 Guenter Bartsch, Heiko Schaefer
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+#
+# benchmark prolog compiler+runtime through kernal
+#
+
+import sys
+import logging
+import codecs
+import cProfile, pstats
+from time import time
+
+from sqlalchemy.orm import sessionmaker
+import model
+
+from zamiaprolog.logicdb       import LogicDB
+from aiprolog.runtime          import AIPrologRuntime, USER_PREFIX
+
+from zamiaprolog.logic         import Predicate
+from zamiaprolog.errors        import PrologError, PrologRuntimeError
+from ai_kernal                 import AIKernal
+from nltools                   import misc
+
+def _bench_fn():
+
+    kernal.compile_module_multi (['base'], False, False, 0)
+
+
+logging.basicConfig(level=logging.INFO)
+# logging.basicConfig(level=logging.DEBUG)
+# logging.getLogger('sqlalchemy.engine').setLevel(logging.WARNING)
+
+kernal = AIKernal()
+
+
+cProfile.run('_bench_fn()', 'mestats')
+
+p = pstats.Stats('mestats')
+# p.strip_dirs().sort_stats(-1).print_stats()
+p.sort_stats('cumulative').print_stats(10)
+

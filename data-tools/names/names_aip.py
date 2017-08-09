4eaf04df150ab726f5135f99d3243c20cc31f2d6
Guenter Bartsch <guenter@zamia.org>
2017-08-09 21:30:11 +0200

    yet another data tool added: generate givennames in AI-Prolog format

---
+++ data-tools/names/names_aip.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+#
+# Copyright 2017 Guenter Bartsch
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# generate AI-Prolog of given names
+#
+
+import codecs
+
+from nltools import misc
+
+misc.init_app ('names_aip')
+
+def name2pred(name):
+
+    res = u''
+    for c in name:
+        if c.isalpha():
+            res += c
+
+    return 'name' + res
+
+for gender in ['Female', 'Male']:
+
+    with codecs.open('%s20.txt' % gender, 'r', 'utf8') as f:
+
+        for line in f:
+
+            name = line.strip()
+
+            print u'rdfsLabel(%s, de, "%s").' % (name2pred(name), name)
+            print u'rdfsLabel(%s, en, "%s").' % (name2pred(name), name)
+            print u'wdpdSexOrGender(%s, wde%s).' % (name2pred(name), gender)
+            print u'wdpdInstanceOf(%s, wde%sGivenName).' % (name2pred(name), gender)
+            print
+

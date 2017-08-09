b136f97faf8039040c8e32252fe86e756efd0519
Guenter Bartsch <guenter@zamia.org>
2017-08-09 20:29:33 +0200

    wd -> data (more generic module name, will collect all sorts of data here)

---
+++ modules/data/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+import logging
+
+DEPENDS    = [ ]
+
+AIP_SOURCES = [
+               'givennames.aip',
+               'wd_sub.pl',
+              ]
+

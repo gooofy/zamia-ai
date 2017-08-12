74007bf8c297a7a52f674511cc4b0d62edda6374
Guenter Bartsch <guenter@zamia.org>
2017-08-12 23:38:17 +0200

    media module ported to Zamia-AI

--- modules/media/__init__.py
+++ modules/media/__init__.py
@@ -3,33 +3,7 @@
 
 DEPENDS    = [ 'base', 'config' ]
 
-PL_SOURCES = [
-              'media.pl',
-             ]
-
-RDF_ALIASES = {
-                u'wde:B5aktuell'             : u'http://www.wikidata.org/entity/Q795291',
-                u'wde:Deutschlandfunk'       : u'http://www.wikidata.org/entity/Q695328',
-                u'wde:SWRAktuell'            : u'http://www.wikidata.org/entity/Q2208796',
-                u'wde:SWR3'                  : u'http://www.wikidata.org/entity/Q2208789',
-                u'wde:PowerHitRadio'         : u'http://www.wikidata.org/entity/Q12372710',
-                u'wde:104.6RTL'              : u'http://www.wikidata.org/entity/Q166266',
-              }
-
-KB_SOURCES = [
-              (
-                [ 
-                  u'wde:B5aktuell',
-                  u'wde:Deutschlandfunk',
-                  u'wde:SWRAktuell',
-                  u'wde:SWR3',
-                  u'wde:PowerHitRadio',
-                  u'wde:104.6RTL', 
-                ],
-                [
-                  [], 
-                ]
-              ),
-              'slots.n3',
-             ]
+AIP_SOURCES = [
+               'slots.aip', 'media.aip',
+              ]
 

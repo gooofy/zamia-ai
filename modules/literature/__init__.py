cdb767dfb2782f33b0538153236e7f417233c627
Guenter Bartsch <guenter@zamia.org>
2017-08-14 18:29:54 +0200

    literature module ported to Zamia-AI

--- modules/literature/__init__.py
+++ modules/literature/__init__.py
@@ -3,30 +3,7 @@
 
 DEPENDS    = [ 'base', 'humans' ]
 
-PL_SOURCES = [
-              'literature.pl'
-             ]
-
-# FIXME: this needs lots of work / expansion
-
-RDF_ALIASES = {
-                u'wde:Novel2001ASpaceOdyssey'     : u'http://www.wikidata.org/entity/Q835341',
-                u'wde:NovelNeuromancer'           : u'http://www.wikidata.org/entity/Q662029',
-                u'wde:NovelTheStand'              : u'http://www.wikidata.org/entity/Q149552',
-              }
-
-KB_SOURCES = [
-
-              (
-                [
-                  u'wde:Novel2001ASpaceOdyssey',
-                  u'wde:NovelNeuromancer',
-                  u'wde:NovelTheStand',
-                ],
-                [
-                  [u'wdpd:Genre'],
-                  [u'wdpd:Author'],
-                ]
-              ),
-            ]
+AIP_SOURCES = [
+               'literature.aip'
+              ]
 

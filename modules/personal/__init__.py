c8df72d620ebcdc07afc1a1183704d18f2eaeadd
Guenter Bartsch <guenter@zamia.org>
2017-11-27 00:27:24 +0100

    renamed personality -> personal, standardize module names

---
+++ modules/personal/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+DEPENDS     = [ 'config', 'data', 'base', 'tech' ]
+
+AIP_SOURCES = [
+               'ai.aip',       'humor.aip', 'personal.aip',  'sex.aip',
+               'culture.aip',  'lang.aip',  'mythology.aip', 'user.aip', 
+               'social.aip',
+              ]
--- modules/personality/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- 
-
-DEPENDS    = [ 'config', 'data', 'base', 'tech' ]
-
-AIP_SOURCES = [
-               'personality.aip', 'user.aip', 'religion.aip', 'sex.aip'
-              ]
-

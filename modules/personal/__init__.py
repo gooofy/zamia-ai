14ab4422fa1e5f029b9fdbc0d3b3578afcc3b110
Guenter Bartsch <guenter@zamia.org>
2017-11-28 03:04:22 +0100

    opinion module added

--- modules/personal/__init__.py
+++ modules/personal/__init__.py
@@ -7,4 +7,6 @@ AIP_SOURCES = [
                'personal.aip', 'humor.aip',   'private.aip',
                'culture.aip',  'lang.aip',    'mythology.aip', 
                'name.aip',     'user.aip',    'social.aip',
+               'rec.aip',      'food.aip',    'opinion.aip',
               ]
+

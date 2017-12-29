a28eb9870ba7ca54122664c07f04dfeb00520f47
Guenter Bartsch <guenter@zamia.org>
2017-12-29 23:35:49 +0100

    aiprolog -> regular prolog

--- modules/config/config.pl
+++ modules/config/config.pl
@@ -1,12 +1,25 @@
 %prolog
 
-rdfsLabel(self, en, "HAL 9000").
-rdfsLabel(self, de, "HAL 9000").
+:- multifile rdfsLabel/3.
+:- multifile forename/3.
+:- multifile wdpdInstanceOf/2.
+:- multifile wdpdSexOrGender/2.
+:- multifile wdpdLocatedIn/2.
+:- multifile favMovie/2.
+:- multifile favStation/2.
+:- multifile favAuthor/2.
+:- multifile favBook/2.
+:- multifile idol/2.
+:- multifile wdpdDateOfBirth/2.
+:- multifile wdpdPlaceOfBirth/2.
 
-forename(self, en, "HAL").
-forename(self, de, "HAL").
-forename(self, en, "Computer").
-forename(self, de, "Computer").
+rdfsLabel(self, en, 'HAL 9000').
+rdfsLabel(self, de, 'HAL 9000').
+ 
+forename(self, en, 'HAL').
+forename(self, de, 'HAL').
+forename(self, en, 'Computer').
+forename(self, de, 'Computer').
 
 wdpdInstanceOf(self, wdeComputer).
 wdpdSexOrGender(self, wdeMale).
@@ -16,6 +29,6 @@ favStation(self, wdeB5Aktuell).
 favAuthor(self, wdeArthurCClarke).
 favBook(self, wde2001ASpaceOdyssey1).
 idol(self, wdeNiklausWirth).
-wdpdDateOfBirth(self, "2017-01-07T17:42:32+00:00").
+wdpdDateOfBirth(self, '2017-01-07T17:42:32+00:00').
 wdpdPlaceOfBirth(self, wdeStuttgart).
 

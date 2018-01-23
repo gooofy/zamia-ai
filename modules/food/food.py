8bf48f9b897a82cddd805bbf6c82ff12af93a661
Guenter Bartsch <guenter@zamia.org>
2018-01-24 00:17:14 +0100

    food module ported

--- modules/food/food.py
+++ modules/food/food.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 
 #
 # Copyright 2016, 2017, 2018 Guenter Bartsch
@@ -18,10 +18,10 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-DEPENDS    = [ 'base', 'dialog' ]
-
-import food
-
 def get_data(k):
-    food.get_data(k)
+
+    k.dte.set_prefixes([u'{self_address:W} '])
+
+    k.dte.dt('en', u"what is wine", u"An alcoholic drink made from fermented grape juice.")
+    k.dte.dt('de', u"was ist wein", u"Ein alkoholisches Getränk aus fermentiertem Traubensaft.")
 

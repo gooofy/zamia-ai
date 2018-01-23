e34ba8f629372718664899898d3e289681fb81f3
Guenter Bartsch <guenter@zamia.org>
2018-01-24 00:13:53 +0100

    language module ported

--- modules/language/languages.py
+++ modules/language/languages.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 
 #
 # Copyright 2016, 2017, 2018 Guenter Bartsch
@@ -18,10 +18,17 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-DEPENDS    = [ 'base', 'dialog' ]
-
-import languages
-
 def get_data(k):
-    languages.get_data(k)
+
+    k.dte.set_prefixes([u'{self_address:W} '])
+
+    k.dte.dt('en', u"(isn't that a wonderful|wonderful|a very nice|terrible) language",
+                   [u"do you speak any foreign languages?",
+                    u"du you like foreign languages?"])
+    k.dte.dt('de', u"(ist das nicht eine wunderbare|wunderbare|eine sehr schöne|furchtbare) sprache",
+                   [u"Sprichst Du irgendwelche Fremdsprachen?",
+                    u"Magst Du Fremdsprachen?"])
+
+    k.dte.ts('en', 't0100', [(u"terrible language", u"do you speak any foreign languages?")])
+    k.dte.ts('de', 't0101', [(u"wunderbare sprache", u"Magst Du Fremdsprachen?")])
 

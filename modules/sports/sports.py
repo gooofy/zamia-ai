4fcbd3e3f2ff8adcec1cb06398563e68fc44cdb3
Guenter Bartsch <guenter@zamia.org>
2018-01-24 00:15:35 +0100

    sport module ported

--- modules/sports/sports.py
+++ modules/sports/sports.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 
 #
 # Copyright 2016, 2017, 2018 Guenter Bartsch
@@ -18,10 +18,16 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-DEPENDS    = [ 'base', 'dialog' ]
-
-import sports
-
 def get_data(k):
-    sports.get_data(k)
+
+    k.dte.set_prefixes([u'{self_address:W} '])
+
+    k.dte.dt('en', u"(I am a fan of|i like) (hertha|vfl bochum)", u"nobody is perfect.")
+    k.dte.dt('de', u"(ich mag|ich bin fan von) (hertha|vfl bochum)", u"niemand ist perfekt.")
+
+    k.dte.dt('en', u"(I like|) (golf|soccer|swimming|football)", u"Physical activity is important for humans, I've heard.")
+    k.dte.dt('de', u"ich mag (golf|fussball|football|schwimmen)", u"Bewegung ist wichtig für Menschen, habe ich gehört.")
+
+    k.dte.dt('en', u"What about golf?", u"I will not crack that joke here.")
+    k.dte.dt('de', u"wie ist es mit golf", u"Ich bringe jetzt nicht den Witz dazu.")
 

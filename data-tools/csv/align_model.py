8a50f46800650479ec361aa3adbd395eab1bf0af
Guenter Bartsch <guenter@zamia.org>
2018-02-06 02:47:49 +0100

    csv_alignment ported to new keras model

---
+++ data-tools/csv/align_model.py
@@ -0,0 +1,321 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+#
+# Copyright 2018 Guenter Bartsch
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+#
+# train keras module alignment model
+#
+
+import os
+import sys
+import codecs
+import logging
+import numpy as np
+
+from tensorflow import keras
+
+from zamiaai                import model
+from nltools                import misc
+from nltools.tokenizer      import tokenize
+
+# DEBUG_LIMIT        = 5000
+DEBUG_LIMIT        = 0
+LANG               = 'en'
+MODEL_DIR          = 'model'
+_PAD               = '__pad'
+_PAD_ID            = 0
+INPUT_MAX_LEN      = 30
+
+# model / keras
+EMB_DIM            = 32
+DENSE1_DIM         = 32
+DENSE2_DIM         = 32
+EPOCHS             = 100
+BATCH_SIZE         = 512
+VALIDATION_SPLIT   = 0.1
+
+class AlignModel(object):
+
+    def __init__(self, session ):
+        self.session     = session
+       
+        self.keras_weights_fn = '%s/keras_weights.hdf5'  % (MODEL_DIR)
+        self.in_dict_fn       = '%s/in_dict.csv'  % (MODEL_DIR)
+        self.out_dict_fn      = '%s/out_dict.csv' % (MODEL_DIR)
+
+    def _setup_model(self):
+
+        self.keras_model = keras.Sequential()
+        self.keras_model.add(keras.layers.Embedding(len(self.input_dict), EMB_DIM, input_length=INPUT_MAX_LEN))
+        self.keras_model.add(keras.layers.Flatten())
+        self.keras_model.add(keras.layers.Dense(DENSE1_DIM, activation='relu'))
+        self.keras_model.add(keras.layers.Dense(DENSE2_DIM, activation='relu'))
+        self.keras_model.add(keras.layers.Dense(len(self.output_dict), activation='softmax'))
+
+        self.keras_model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+        logging.info(self.keras_model.summary())
+
+    def train(self, num_steps, incremental):
+
+        # load discourses from db
+        
+        logging.info('load discourses from db...')
+
+        self.training_data = []
+        tds      = set()
+        for td in self.session.query(model.TrainingData).filter(model.TrainingData.lang==LANG).filter(model.TrainingData.module!='bots'):
+
+            if td.inp in tds:
+                continue
+            tds.add(td.inp)
+
+            inp = tokenize(td.inp, lang=LANG)
+            if len(inp) > INPUT_MAX_LEN:
+                inp = inp[:INPUT_MAX_LEN]
+
+            self.training_data.append((inp, td.module))
+
+            if DEBUG_LIMIT and len(tds)>DEBUG_LIMIT:
+                break
+
+        #
+        # set up model dir
+        #
+
+        if not incremental:
+            try:
+                shutil.rmtree(MODEL_DIR)
+            except:
+                pass
+
+            misc.mkdirs(MODEL_DIR)
+
+        #
+        # load or create input/output dicts
+        #
+
+        if incremental:
+            logging.info("loading input and output dicts...")
+            self.load_dicts()
+
+        else:
+            logging.info("computing input and output dicts...")
+
+            self.compute_dicts()
+            self.save_dicts()
+
+        #
+        # compute datasets
+        #
+
+        logging.info("computing datasets...")
+
+        train_x = []
+        train_y = []
+
+        cnt = 0
+        for inp, mn in self.training_data:
+
+            x = self.compute_x(inp)
+            y = self.compute_y(mn)
+
+            train_x.append(x)
+            train_y.append(y)
+
+            cnt += 1
+
+        self.train_x = np.array(train_x, np.int32)
+        self.train_y = keras.utils.to_categorical(train_y, len(self.output_dict))
+
+        logging.info("computing datasets done. train:x=%s,y=%s" % (self.train_x.shape, self.train_y.shape))
+
+        #
+        # define the keras model
+        #
+
+        self._setup_model()
+
+        #
+        # fit training data
+        #
+
+        best_loss  = 100.0
+        best_epoch = 0
+
+        for epoch in range(EPOCHS):
+
+            h = self.keras_model.fit(self.train_x, self.train_y, 
+                                     epochs=1, 
+                                     validation_split=VALIDATION_SPLIT, 
+                                     batch_size=BATCH_SIZE)
+
+            cur_loss = h.history['val_loss'][0]
+
+            if cur_loss < best_loss:
+                logging.info("*** BEST LOSS SO FAR IN THIS RUN")
+
+                best_loss  = cur_loss
+                best_epoch = epoch
+
+                # save the result
+
+                self.keras_model.save_weights(self.keras_weights_fn, overwrite=True)
+                logging.info ('%s written.' % self.keras_weights_fn)
+
+    def load(self):
+
+        #
+        # load dicts
+        #
+
+        self.load_dicts()
+
+        #
+        # define the keras model
+        #
+
+        self._setup_model()
+
+        #
+        # load the weights
+        #
+
+        self.keras_model.load_weights(self.keras_weights_fn)
+        logging.info ('%s loaded.' % self.keras_weights_fn)
+
+    def compute_dicts(self):
+
+        # build input and output dicts
+
+        self.input_dict  = {_PAD: _PAD_ID}
+        self.output_dict = {_PAD: _PAD_ID}
+
+        for inp, mn in self.training_data:
+
+            # input
+
+            i = 0
+            for token in inp:
+                if not token in self.input_dict:
+                    self.input_dict[token] = len(self.input_dict)
+
+            # output
+
+            if not mn in self.output_dict:
+                self.output_dict[mn] = len(self.output_dict)
+
+        logging.info ('dicts done. input: %d entries, output: %d entries' %
+                      (len(self.input_dict), len(self.output_dict)))
+
+    def save_dicts(self):
+
+        with codecs.open(self.in_dict_fn, 'w', 'utf8') as f:
+
+            for k in sorted(self.input_dict):
+
+                f.write(u"%d;%s\n" % (self.input_dict[k], k))
+
+        logging.info ('%s written.', self.in_dict_fn)
+
+        with codecs.open(self.out_dict_fn, 'w', 'utf8') as f:
+
+            for k in sorted(self.output_dict):
+
+                f.write(u"%d;%s\n" % (self.output_dict[k], k))
+
+        logging.info ('%s written.', self.out_dict_fn)
+
+    def load_dicts(self):
+
+        with codecs.open(self.in_dict_fn, 'r', 'utf8') as f:
+
+            self.input_dict = {}
+
+            while True:
+                line = f.readline()
+                if not line:
+                    break
+
+                line = line.lstrip().rstrip()
+
+                parts = line.split(';')
+
+                self.input_dict[parts[1]] = int(parts[0])
+
+        logging.info ('%s read, %d entries.' % (self.in_dict_fn, len(self.input_dict)))
+
+        with codecs.open(self.out_dict_fn, 'r', 'utf8') as f:
+
+            self.output_dict = {}
+
+            while True:
+                line = f.readline()
+                if not line:
+                    break
+
+                line = line.lstrip().rstrip()
+
+                parts = line.split(';')
+
+                self.output_dict[parts[1]] = int(parts[0])
+
+        logging.info ('%s read, %d entries.' % (self.out_dict_fn, len(self.output_dict)))
+ 
+    def compute_x(self, inp):
+
+        x = list(map(lambda token: self.input_dict[unicode(token)] if unicode(token) in self.input_dict else _PAD_ID, inp))
+
+        while len(x) < INPUT_MAX_LEN:
+            x.append(_PAD_ID)
+
+        if len(x) > INPUT_MAX_LEN:
+            x = x[:INPUT_MAX_LEN]
+
+        return x
+
+    def compute_y(self, mn):
+        y = self.output_dict[mn] if mn in self.output_dict else PAD_ID
+
+        return y
+
+    def predict(self, inp):
+
+        x = np.array([self.compute_x(tokenize(inp, lang=LANG))], np.int32)
+
+        y = self.keras_model.predict(x)
+
+        # logging.info('%s -> %s' % (x, y))
+
+        y = np.argmax(y)
+
+        # logging.info ('argmax: %s' % y)
+
+        # import pdb; pdb.set_trace()
+
+        mn = None
+        for m in self.output_dict:
+            if self.output_dict[m] == y:
+                mn = m
+                break
+
+        # logging.info ('mn: %s' % mn)
+
+        return mn
+
--- data-tools/csv/csv_align.py
+++ data-tools/csv/csv_align.py
@@ -33,28 +33,10 @@ import numpy as np
 
 from optparse               import OptionParser
 from gensim.models          import word2vec
-from zamiaai                import model
 from nltools                import misc, tokenizer
-from sqlalchemy.orm         import sessionmaker
-from scipy.spatial.distance import cosine
+from align_model            import AlignModel
 
-DEFAULT_LOGLEVEL   = logging.DEBUG
-DEFAULT_OUTPUT     = 'foo.aip'
-DEBUG_LIMIT        = 1000
-
-def avg_feature_vector(words, model, num_features, index2word_set):
-        #function to average all words vectors in a given paragraph
-        featureVec = np.zeros((num_features,), dtype="float32")
-        nwords = 0
-
-        for word in words:
-            if word in index2word_set:
-                nwords = nwords+1
-                featureVec = np.add(featureVec, model[word])
-
-        if nwords>0:
-            featureVec = np.divide(featureVec, nwords)
-        return featureVec
+OUTPUT_DIR  = 'out'
 
 #
 # init, cmdline
@@ -81,71 +63,11 @@ if len(args) != 1:
 csvfn   = args[0]
 
 #
-# db
+# load model
 #
 
-Session = sessionmaker(bind=model.engine)
-session = Session()
-
-#
-# vectors
-#
-
-time_start = time.time()
-WORD2VEC_MODEL = '/home/bofh/projects/ai/data/word2vec/GoogleNews-vectors-negative300.bin'
-
-logging.info('loading %s...' % WORD2VEC_MODEL)
-
-# word2vec_model = word2vec.Word2Vec.load_word2vec_format(WORD2VEC_MODEL, binary=True)
-# 
-# #list containing names of words in the vocabulary
-# index2word_set = set(word2vec_model.index2word)
-
-logging.info('loading %s... done. took %fs' % (WORD2VEC_MODEL, time.time()-time_start))
-
-#
-# pre-compute training data vectors
-#
-
-time_start = time.time()
-cnt = 0
-
-logging.info ('precomputing training data word vectors...')
-
-td_covered = set()
-td_vectors = []
-
-for td in session.query(model.TrainingData).filter(model.TrainingData.lang=='en'):
-
-    import pdb; pdb.set_trace()
-
-    if td.inp in td_covered:
-        continue
-    td_covered.add(td.inp)
-
-    try:
-
-        sentence_2 = tokenizer.tokenize(td.inp, lang='en')
-        sentence_2_avg_vector = avg_feature_vector(sentence_2, model=word2vec_model, num_features=300, index2word_set=index2word_set)
-
-        td_vectors.append((td.module, sentence_2_avg_vector))
-
-        # logging.debug ('%6d: vector for %s is %s' % (cnt, repr(sentence_2), repr(sentence_2_avg_vector)))
-
-        cnt += 1
-
-        if DEBUG_LIMIT and cnt>DEBUG_LIMIT:
-            break
-
-        if cnt % 1000 == 0:
-            logging.info ('   %6d vectors computed in %fs.' % (cnt, time.time()-time_start))
-
-    except:
-        logging.error(traceback.format_exc())
-
-logging.info ('precomputing training data word vectors...done. %d vectors computed in %fs.' % (cnt, time.time()-time_start))
-
-sys.exit(0)
+align_model = AlignModel(None)
+align_model.load()
 
 #
 # parse CSV input file, map uncovered questions to module:sourcefile keys
@@ -153,7 +75,7 @@ sys.exit(0)
 
 logging.info('parsing %s ...' % csvfn)
 
-map_dict = {} # key -> {ques_en -> (resp_en, ques_de, resp_de)}
+map_dict = {} # module -> {inp_en -> (resp_en, inp_de, resp_de)}
 
 with codecs.open(csvfn, 'r', 'utf8') as csvf:
 
@@ -168,66 +90,38 @@ with codecs.open(csvfn, 'r', 'utf8') as csvf:
             logging.error('failed to parse line: %s' % line.strip())
             continue
 
-        ques_en = parts[0]
+        inp_en  = parts[0]
         resp_en = parts[1]
-        ques_de = parts[2]
+        inp_de  = parts[2]
         resp_de = parts[3]
 
-        ques_en_t = tokenizer.tokenize(ques_en, lang='en')
+        mn = align_model.predict(inp_en)
 
-        ques_en_t = u' '.join(ques_en_t)
+        if not mn in map_dict:
+            map_dict[mn] = {}
 
-        if ques_en_t in td_covered:
-            logging.debug (u'%6d ques_en: %s already covered. next.' % (cnt, ques_en))
-            continue
+        # logging.debug (u'%6d inp_en: %s -> %s' % (cnt, inp_en, mn))
+        map_dict[mn][inp_en] = (resp_en, inp_de, resp_de)
 
-        ques_en_avg_vector = avg_feature_vector(ques_en_t, model=word2vec_model, num_features=300, index2word_set=index2word_set)
-
-        # logging.debug("ques_en_avg_vector: %s" % repr(ques_en_avg_vector))
-
-        best_sim = 0.0
-        best_key = 'misc'
-
-        for key, td_v in td_vectors:
-
-            try:
-
-                sen1_sen2_similarity =  1 - cosine(ques_en_avg_vector, td_v)
-
-                if sen1_sen2_similarity > best_sim:
-                    best_sim = sen1_sen2_similarity
-                    best_key = key
-
-            except:
-                logging.error(traceback.format_exc())
-
-        if not best_key in map_dict:
-            map_dict[best_key] = {}
-
-        logging.debug (u'%6d ques_en: %s -> %s %f' % (cnt, ques_en, best_key, best_sim))
-        map_dict[best_key][ques_en] = (resp_en, ques_de, resp_de)
-
-        # if cnt > 50:
-        #     break
 
 # print repr(map_dict)
 
 #
-# generate output AI-Prolog source code
+# generate output files
 #
 
-with codecs.open(DEFAULT_OUTPUT, 'w', 'utf8') as outf:
 
-    for key in map_dict:
+for mn in map_dict:
 
-        outf.write(u'\n%%\n%% %s\n%%\n\n' % key)
+    outfn = '%s/%s.csv' % (OUTPUT_DIR, mn)
+    with codecs.open(outfn, 'w', 'utf8') as outf:
 
-        for ques_en in sorted(map_dict[key]):
+        for ques_en in sorted(map_dict[mn]):
 
-            resp_en, ques_de, resp_de = map_dict[key][ques_en]
+            resp_en, ques_de, resp_de = map_dict[mn][ques_en]
 
-            outf.write('train(en) :- "%s",\n             "%s".\n'   % (ques_en, resp_en))
-            outf.write('train(de) :- "%s",\n             "%s".\n\n' % (ques_de, resp_de))
+            line = u"%s;%s;%s;%s" % (ques_en, resp_en, ques_de, resp_de)
+            outf.write(u"%s\n" % line)
 
-logging.info ('%s written.' % DEFAULT_OUTPUT)
+    logging.info ('%s written.' % outfn)
 

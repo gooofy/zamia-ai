79a26de7968cf21cbd2e8fc73e3eb20bbeb3da89
Guenter Bartsch <guenter@zamia.org>
2018-03-19 15:46:47 +0100

    fix quotes

--- modules/data/givennames20.pl
+++ modules/data/givennames20.pl
@@ -4,93 +4,93 @@
 :- multifile wdpdInstanceOf/2.
 :- multifile wdpdSexOrGender/2.
 
-rdfsLabel(nameBarbara, de, 'Barbara').
-rdfsLabel(nameBarbara, en, 'Barbara').
-rdfsLabel(nameBrigitte, de, 'Brigitte').
-rdfsLabel(nameBrigitte, en, 'Brigitte').
-rdfsLabel(nameCharles, de, 'Charles').
-rdfsLabel(nameCharles, en, 'Charles').
-rdfsLabel(nameDavid, de, 'David').
-rdfsLabel(nameDavid, en, 'David').
-rdfsLabel(nameDorothy, de, 'Dorothy').
-rdfsLabel(nameDorothy, en, 'Dorothy').
-rdfsLabel(nameElizabeth, de, 'Elizabeth').
-rdfsLabel(nameElizabeth, en, 'Elizabeth').
-rdfsLabel(nameGünter, de, 'Günter').
-rdfsLabel(nameGünter, en, 'Günter').
-rdfsLabel(nameHeiko, de, 'Heiko').
-rdfsLabel(nameHeiko, en, 'Heiko').
-rdfsLabel(nameHeinz, de, 'Heinz').
-rdfsLabel(nameHeinz, en, 'Heinz').
-rdfsLabel(nameHelga, de, 'Helga').
-rdfsLabel(nameHelga, en, 'Helga').
-rdfsLabel(nameHelmut, de, 'Helmut').
-rdfsLabel(nameHelmut, en, 'Helmut').
-rdfsLabel(nameJames, de, 'James').
-rdfsLabel(nameJames, en, 'James').
-rdfsLabel(nameJennifer, de, 'Jennifer').
-rdfsLabel(nameJennifer, en, 'Jennifer').
-rdfsLabel(nameJohn, de, 'John').
-rdfsLabel(nameJohn, en, 'John').
-rdfsLabel(nameJoseph, de, 'Joseph').
-rdfsLabel(nameJoseph, en, 'Joseph').
-rdfsLabel(nameJürgen, de, 'Jürgen').
-rdfsLabel(nameJürgen, en, 'Jürgen').
-rdfsLabel(nameKarin, de, 'Karin').
-rdfsLabel(nameKarin, en, 'Karin').
-rdfsLabel(nameKlaus, de, 'Klaus').
-rdfsLabel(nameKlaus, en, 'Klaus').
-rdfsLabel(nameLinda, de, 'Linda').
-rdfsLabel(nameLinda, en, 'Linda').
-rdfsLabel(nameLisa, de, 'Lisa').
-rdfsLabel(nameLisa, en, 'Lisa').
-rdfsLabel(nameManfred, de, 'Manfred').
-rdfsLabel(nameManfred, en, 'Manfred').
-rdfsLabel(nameMargaret, de, 'Margaret').
-rdfsLabel(nameMargaret, en, 'Margaret').
-rdfsLabel(nameMaria, de, 'Maria').
-rdfsLabel(nameMaria, en, 'Maria').
-rdfsLabel(nameMary, de, 'Mary').
-rdfsLabel(nameMary, en, 'Mary').
-rdfsLabel(nameMichael, de, 'Michael').
-rdfsLabel(nameMichael, en, 'Michael').
-rdfsLabel(nameMonika, de, 'Monika').
-rdfsLabel(nameMonika, en, 'Monika').
-rdfsLabel(nameNancy, de, 'Nancy').
-rdfsLabel(nameNancy, en, 'Nancy').
-rdfsLabel(namePatricia, de, 'Patricia').
-rdfsLabel(namePatricia, en, 'Patricia').
-rdfsLabel(namePeter, de, 'Peter').
-rdfsLabel(namePeter, en, 'Peter').
-rdfsLabel(namePetra, de, 'Petra').
-rdfsLabel(namePetra, en, 'Petra').
-rdfsLabel(nameRenate, de, 'Renate').
-rdfsLabel(nameRenate, en, 'Renate').
-rdfsLabel(nameRichard, de, 'Richard').
-rdfsLabel(nameRichard, en, 'Richard').
-rdfsLabel(nameRobert, de, 'Robert').
-rdfsLabel(nameRobert, en, 'Robert').
-rdfsLabel(nameSabine, de, 'Sabine').
-rdfsLabel(nameSabine, en, 'Sabine').
-rdfsLabel(nameSusan, de, 'Susan').
-rdfsLabel(nameSusan, en, 'Susan').
-rdfsLabel(nameThomas, de, 'Thomas').
-rdfsLabel(nameThomas, en, 'Thomas').
-rdfsLabel(nameUrsula, de, 'Ursula').
-rdfsLabel(nameUrsula, en, 'Ursula').
-rdfsLabel(nameWerner, de, 'Werner').
-rdfsLabel(nameWerner, en, 'Werner').
-rdfsLabel(nameWilliam, de, 'William').
-rdfsLabel(nameWilliam, en, 'William').
-rdfsLabel(nameWolfgang, de, 'Wolfgang').
-rdfsLabel(nameWolfgang, en, 'Wolfgang').
+rdfsLabel(nameBarbara, de, "Barbara").
+rdfsLabel(nameBarbara, en, "Barbara").
+rdfsLabel(nameBrigitte, de, "Brigitte").
+rdfsLabel(nameBrigitte, en, "Brigitte").
+rdfsLabel(nameCharles, de, "Charles").
+rdfsLabel(nameCharles, en, "Charles").
+rdfsLabel(nameDavid, de, "David").
+rdfsLabel(nameDavid, en, "David").
+rdfsLabel(nameDorothy, de, "Dorothy").
+rdfsLabel(nameDorothy, en, "Dorothy").
+rdfsLabel(nameElizabeth, de, "Elizabeth").
+rdfsLabel(nameElizabeth, en, "Elizabeth").
+rdfsLabel('nameGünter', de, "Günter").
+rdfsLabel('nameGünter', en, "Günter").
+rdfsLabel(nameHeiko, de, "Heiko").
+rdfsLabel(nameHeiko, en, "Heiko").
+rdfsLabel(nameHeinz, de, "Heinz").
+rdfsLabel(nameHeinz, en, "Heinz").
+rdfsLabel(nameHelga, de, "Helga").
+rdfsLabel(nameHelga, en, "Helga").
+rdfsLabel(nameHelmut, de, "Helmut").
+rdfsLabel(nameHelmut, en, "Helmut").
+rdfsLabel(nameJames, de, "James").
+rdfsLabel(nameJames, en, "James").
+rdfsLabel(nameJennifer, de, "Jennifer").
+rdfsLabel(nameJennifer, en, "Jennifer").
+rdfsLabel(nameJohn, de, "John").
+rdfsLabel(nameJohn, en, "John").
+rdfsLabel(nameJoseph, de, "Joseph").
+rdfsLabel(nameJoseph, en, "Joseph").
+rdfsLabel('nameJürgen', de, "Jürgen").
+rdfsLabel('nameJürgen', en, "Jürgen").
+rdfsLabel(nameKarin, de, "Karin").
+rdfsLabel(nameKarin, en, "Karin").
+rdfsLabel(nameKlaus, de, "Klaus").
+rdfsLabel(nameKlaus, en, "Klaus").
+rdfsLabel(nameLinda, de, "Linda").
+rdfsLabel(nameLinda, en, "Linda").
+rdfsLabel(nameLisa, de, "Lisa").
+rdfsLabel(nameLisa, en, "Lisa").
+rdfsLabel(nameManfred, de, "Manfred").
+rdfsLabel(nameManfred, en, "Manfred").
+rdfsLabel(nameMargaret, de, "Margaret").
+rdfsLabel(nameMargaret, en, "Margaret").
+rdfsLabel(nameMaria, de, "Maria").
+rdfsLabel(nameMaria, en, "Maria").
+rdfsLabel(nameMary, de, "Mary").
+rdfsLabel(nameMary, en, "Mary").
+rdfsLabel(nameMichael, de, "Michael").
+rdfsLabel(nameMichael, en, "Michael").
+rdfsLabel(nameMonika, de, "Monika").
+rdfsLabel(nameMonika, en, "Monika").
+rdfsLabel(nameNancy, de, "Nancy").
+rdfsLabel(nameNancy, en, "Nancy").
+rdfsLabel(namePatricia, de, "Patricia").
+rdfsLabel(namePatricia, en, "Patricia").
+rdfsLabel(namePeter, de, "Peter").
+rdfsLabel(namePeter, en, "Peter").
+rdfsLabel(namePetra, de, "Petra").
+rdfsLabel(namePetra, en, "Petra").
+rdfsLabel(nameRenate, de, "Renate").
+rdfsLabel(nameRenate, en, "Renate").
+rdfsLabel(nameRichard, de, "Richard").
+rdfsLabel(nameRichard, en, "Richard").
+rdfsLabel(nameRobert, de, "Robert").
+rdfsLabel(nameRobert, en, "Robert").
+rdfsLabel(nameSabine, de, "Sabine").
+rdfsLabel(nameSabine, en, "Sabine").
+rdfsLabel(nameSusan, de, "Susan").
+rdfsLabel(nameSusan, en, "Susan").
+rdfsLabel(nameThomas, de, "Thomas").
+rdfsLabel(nameThomas, en, "Thomas").
+rdfsLabel(nameUrsula, de, "Ursula").
+rdfsLabel(nameUrsula, en, "Ursula").
+rdfsLabel(nameWerner, de, "Werner").
+rdfsLabel(nameWerner, en, "Werner").
+rdfsLabel(nameWilliam, de, "William").
+rdfsLabel(nameWilliam, en, "William").
+rdfsLabel(nameWolfgang, de, "Wolfgang").
+rdfsLabel(nameWolfgang, en, "Wolfgang").
 wdpdInstanceOf(nameBarbara, wdeFemaleGivenName).
 wdpdInstanceOf(nameBrigitte, wdeFemaleGivenName).
 wdpdInstanceOf(nameCharles, wdeMaleGivenName).
 wdpdInstanceOf(nameDavid, wdeMaleGivenName).
 wdpdInstanceOf(nameDorothy, wdeFemaleGivenName).
 wdpdInstanceOf(nameElizabeth, wdeFemaleGivenName).
-wdpdInstanceOf(nameGünter, wdeMaleGivenName).
+wdpdInstanceOf('nameGünter', wdeMaleGivenName).
 wdpdInstanceOf(nameHeiko, wdeMaleGivenName).
 wdpdInstanceOf(nameHeinz, wdeMaleGivenName).
 wdpdInstanceOf(nameHelga, wdeFemaleGivenName).
@@ -99,7 +99,7 @@ wdpdInstanceOf(nameJames, wdeMaleGivenName).
 wdpdInstanceOf(nameJennifer, wdeFemaleGivenName).
 wdpdInstanceOf(nameJohn, wdeMaleGivenName).
 wdpdInstanceOf(nameJoseph, wdeMaleGivenName).
-wdpdInstanceOf(nameJürgen, wdeMaleGivenName).
+wdpdInstanceOf('nameJürgen', wdeMaleGivenName).
 wdpdInstanceOf(nameKarin, wdeFemaleGivenName).
 wdpdInstanceOf(nameKlaus, wdeMaleGivenName).
 wdpdInstanceOf(nameLinda, wdeFemaleGivenName).
@@ -130,7 +130,7 @@ wdpdSexOrGender(nameCharles, wdeMale).
 wdpdSexOrGender(nameDavid, wdeMale).
 wdpdSexOrGender(nameDorothy, wdeFemale).
 wdpdSexOrGender(nameElizabeth, wdeFemale).
-wdpdSexOrGender(nameGünter, wdeMale).
+wdpdSexOrGender('nameGünter', wdeMale).
 wdpdSexOrGender(nameHeiko, wdeMale).
 wdpdSexOrGender(nameHeinz, wdeMale).
 wdpdSexOrGender(nameHelga, wdeFemale).
@@ -139,7 +139,7 @@ wdpdSexOrGender(nameJames, wdeMale).
 wdpdSexOrGender(nameJennifer, wdeFemale).
 wdpdSexOrGender(nameJohn, wdeMale).
 wdpdSexOrGender(nameJoseph, wdeMale).
-wdpdSexOrGender(nameJürgen, wdeMale).
+wdpdSexOrGender('nameJürgen', wdeMale).
 wdpdSexOrGender(nameKarin, wdeFemale).
 wdpdSexOrGender(nameKlaus, wdeMale).
 wdpdSexOrGender(nameLinda, wdeFemale).

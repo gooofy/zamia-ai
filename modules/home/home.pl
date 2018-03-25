d72b054e50e531d7347c92dd77960773b158b14f
Guenter Bartsch <guenter@zamia.org>
2018-03-25 01:54:50 +0100

    adapt home module to new interfaces

--- modules/home/home.pl
+++ modules/home/home.pl
@@ -22,68 +22,68 @@ aiHomeLocation(aiHLStudy).
 aiHomeLocation(aiHLUpstairs).
 aiHomeLocation(aiHLWorkRoom).
 aiHomeLocation(aiHLWorkshop).
-aiPrepLoc(aiHLAttic, de, 'auf dem').
-aiPrepLoc(aiHLAttic, en, 'in the').
-aiPrepLoc(aiHLBasement, de, 'im').
-aiPrepLoc(aiHLBasement, en, 'in the').
-aiPrepLoc(aiHLBathroom, de, 'im').
-aiPrepLoc(aiHLBathroom, en, 'in the').
-aiPrepLoc(aiHLBedroom, de, 'im').
-aiPrepLoc(aiHLBedroom, en, 'in the').
-aiPrepLoc(aiHLChildrensRoom, de, 'im').
-aiPrepLoc(aiHLChildrensRoom, en, 'in the').
-aiPrepLoc(aiHLDiningRoom, de, 'im').
-aiPrepLoc(aiHLDiningRoom, en, 'in the').
-aiPrepLoc(aiHLGuestRoom, de, 'im').
-aiPrepLoc(aiHLGuestRoom, en, 'in the').
-aiPrepLoc(aiHLHall, de, 'im').
-aiPrepLoc(aiHLHall, en, 'in the').
-aiPrepLoc(aiHLKitchen, de, 'in der').
-aiPrepLoc(aiHLKitchen, en, 'in the').
-aiPrepLoc(aiHLLivingRoom, de, 'im').
-aiPrepLoc(aiHLLivingRoom, en, 'in the').
-aiPrepLoc(aiHLOffice, de, 'im').
-aiPrepLoc(aiHLOffice, en, 'in the').
-aiPrepLoc(aiHLShop, de, 'im').
-aiPrepLoc(aiHLShop, en, 'in the').
-aiPrepLoc(aiHLStudy, de, 'im').
-aiPrepLoc(aiHLStudy, en, 'in the').
-aiPrepLoc(aiHLUpstairs, de, 'im').
-aiPrepLoc(aiHLUpstairs, en, '').
-aiPrepLoc(aiHLWorkRoom, de, 'im').
-aiPrepLoc(aiHLWorkRoom, en, 'in the').
-aiPrepLoc(aiHLWorkshop, de, 'in der').
-aiPrepLoc(aiHLWorkshop, en, 'in the').
-rdfsLabel(aiHLAttic, de, 'Dachboden').
-rdfsLabel(aiHLAttic, en, 'attic').
-rdfsLabel(aiHLBasement, de, 'Keller').
-rdfsLabel(aiHLBasement, en, 'basement').
-rdfsLabel(aiHLBasement, en, 'cellar').
-rdfsLabel(aiHLBathroom, de, 'Badezimmer').
-rdfsLabel(aiHLBathroom, en, 'bathroom').
-rdfsLabel(aiHLBedroom, de, 'Schlafzimmer').
-rdfsLabel(aiHLBedroom, en, 'bedroom').
-rdfsLabel(aiHLChildrensRoom, de, 'Kinderzimmer').
-rdfsLabel(aiHLChildrensRoom, en, 'children\'s room').
-rdfsLabel(aiHLDiningRoom, de, 'Esszimmer').
-rdfsLabel(aiHLDiningRoom, en, 'dining room').
-rdfsLabel(aiHLGuestRoom, de, 'Gästezimmer').
-rdfsLabel(aiHLGuestRoom, en, 'guest room').
-rdfsLabel(aiHLHall, de, 'Flur').
-rdfsLabel(aiHLHall, en, 'hall').
-rdfsLabel(aiHLKitchen, de, 'Küche').
-rdfsLabel(aiHLKitchen, en, 'kitchen').
-rdfsLabel(aiHLLivingRoom, de, 'Wohnzimmer').
-rdfsLabel(aiHLLivingRoom, en, 'living room').
-rdfsLabel(aiHLOffice, de, 'Büro').
-rdfsLabel(aiHLOffice, en, 'office').
-rdfsLabel(aiHLShop, de, 'Laden').
-rdfsLabel(aiHLShop, en, 'shop').
-rdfsLabel(aiHLStudy, de, 'Arbeitszimmer').
-rdfsLabel(aiHLStudy, en, 'study').
-rdfsLabel(aiHLUpstairs, de, 'Obergeschoss').
-rdfsLabel(aiHLUpstairs, en, 'upstairs').
-rdfsLabel(aiHLWorkRoom, de, 'Arbeitszimmer').
-rdfsLabel(aiHLWorkRoom, en, 'work room').
-rdfsLabel(aiHLWorkshop, de, 'Werkstatt').
-rdfsLabel(aiHLWorkshop, en, 'workshop').
+aiPrepLoc(aiHLAttic, de, "auf dem").
+aiPrepLoc(aiHLAttic, en, "in the").
+aiPrepLoc(aiHLBasement, de, "im").
+aiPrepLoc(aiHLBasement, en, "in the").
+aiPrepLoc(aiHLBathroom, de, "im").
+aiPrepLoc(aiHLBathroom, en, "in the").
+aiPrepLoc(aiHLBedroom, de, "im").
+aiPrepLoc(aiHLBedroom, en, "in the").
+aiPrepLoc(aiHLChildrensRoom, de, "im").
+aiPrepLoc(aiHLChildrensRoom, en, "in the").
+aiPrepLoc(aiHLDiningRoom, de, "im").
+aiPrepLoc(aiHLDiningRoom, en, "in the").
+aiPrepLoc(aiHLGuestRoom, de, "im").
+aiPrepLoc(aiHLGuestRoom, en, "in the").
+aiPrepLoc(aiHLHall, de, "im").
+aiPrepLoc(aiHLHall, en, "in the").
+aiPrepLoc(aiHLKitchen, de, "in der").
+aiPrepLoc(aiHLKitchen, en, "in the").
+aiPrepLoc(aiHLLivingRoom, de, "im").
+aiPrepLoc(aiHLLivingRoom, en, "in the").
+aiPrepLoc(aiHLOffice, de, "im").
+aiPrepLoc(aiHLOffice, en, "in the").
+aiPrepLoc(aiHLShop, de, "im").
+aiPrepLoc(aiHLShop, en, "in the").
+aiPrepLoc(aiHLStudy, de, "im").
+aiPrepLoc(aiHLStudy, en, "in the").
+aiPrepLoc(aiHLUpstairs, de, "im").
+aiPrepLoc(aiHLUpstairs, en, "").
+aiPrepLoc(aiHLWorkRoom, de, "im").
+aiPrepLoc(aiHLWorkRoom, en, "in the").
+aiPrepLoc(aiHLWorkshop, de, "in der").
+aiPrepLoc(aiHLWorkshop, en, "in the").
+rdfsLabel(aiHLAttic, de, "Dachboden").
+rdfsLabel(aiHLAttic, en, "attic").
+rdfsLabel(aiHLBasement, de, "Keller").
+rdfsLabel(aiHLBasement, en, "basement").
+rdfsLabel(aiHLBasement, en, "cellar").
+rdfsLabel(aiHLBathroom, de, "Badezimmer").
+rdfsLabel(aiHLBathroom, en, "bathroom").
+rdfsLabel(aiHLBedroom, de, "Schlafzimmer").
+rdfsLabel(aiHLBedroom, en, "bedroom").
+rdfsLabel(aiHLChildrensRoom, de, "Kinderzimmer").
+rdfsLabel(aiHLChildrensRoom, en, "children\"s room").
+rdfsLabel(aiHLDiningRoom, de, "Esszimmer").
+rdfsLabel(aiHLDiningRoom, en, "dining room").
+rdfsLabel(aiHLGuestRoom, de, "Gästezimmer").
+rdfsLabel(aiHLGuestRoom, en, "guest room").
+rdfsLabel(aiHLHall, de, "Flur").
+rdfsLabel(aiHLHall, en, "hall").
+rdfsLabel(aiHLKitchen, de, "Küche").
+rdfsLabel(aiHLKitchen, en, "kitchen").
+rdfsLabel(aiHLLivingRoom, de, "Wohnzimmer").
+rdfsLabel(aiHLLivingRoom, en, "living room").
+rdfsLabel(aiHLOffice, de, "Büro").
+rdfsLabel(aiHLOffice, en, "office").
+rdfsLabel(aiHLShop, de, "Laden").
+rdfsLabel(aiHLShop, en, "shop").
+rdfsLabel(aiHLStudy, de, "Arbeitszimmer").
+rdfsLabel(aiHLStudy, en, "study").
+rdfsLabel(aiHLUpstairs, de, "Obergeschoss").
+rdfsLabel(aiHLUpstairs, en, "upstairs").
+rdfsLabel(aiHLWorkRoom, de, "Arbeitszimmer").
+rdfsLabel(aiHLWorkRoom, en, "work room").
+rdfsLabel(aiHLWorkshop, de, "Werkstatt").
+rdfsLabel(aiHLWorkshop, en, "workshop").
--- modules/home/home.py
+++ modules/home/home.py
@@ -17,6 +17,8 @@
 # limitations under the License.
 #
 
+from xsbprolog import XSBString
+
 def get_data(k):
 
     k.dte.set_prefixes([u''])
@@ -25,9 +27,9 @@ def get_data(k):
 
     for lang in ['en', 'de']:
         for res in k.prolog_query("aiHomeLocation(HOME_LOCATION), rdfsLabel(HOME_LOCATION, %s, LABEL), aiPrepLoc(HOME_LOCATION, %s, PL)." % (lang, lang)):
-            s_loc   = res[0] 
-            s_label = res[1]
-            s_pl    = res[2]
+            s_loc   = res[0].name
+            s_label = res[1].value
+            s_pl    = res[2].value if isinstance(res[2], XSBString) else u""
             k.dte.ner(lang, 'home_location', s_loc, s_label)
             k.dte.macro(lang, 'home_locations', {'LABEL': s_label, 'PL': s_pl})
 
@@ -41,7 +43,7 @@ def get_data(k):
 
         def act(c, args):
             laction, loc = args
-            c.kernal.mem_set(c.realm, 'action', laction)
+            c.kernal.mem_set(c.realm, 'action', XSBString(laction))
             c.kernal.mem_push(c.user, 'f1loc', loc)
 
         if ts>=0:
@@ -61,22 +63,23 @@ def get_data(k):
 
     def check_light (c, args):
         action, loc = args
-        assert c.kernal.mem_get(c.realm, 'action') == action
-        l1, score = c.kernal.mem_get_multi(c.user, 'f1loc')[0] 
-        assert l1 == loc
+        # import pdb; pdb.set_trace()
+        assert c.kernal.mem_get(c.realm, 'action').value == action
+        l1, score = c.kernal.mem_get_multi(c.user, 'f1loc')[0]
+        assert l1.name == loc
         
-    k.dte.ts('en', 't0000', [(u"computer, please switch on the lights in the living room", u"", check_light, ['light_on', 'aiHLLivingRoom'])])
-    k.dte.ts('de', 't0001', [(u"Computer, bitte schalte das Licht in der Werkstatt ein", u"", check_light, ['light_on', 'aiHLWorkshop'])])
+    k.dte.ts('en', 't0000', [(u"please switch on the lights in the living room", u"", check_light, ['light_on', 'aiHLLivingRoom'])])
+    k.dte.ts('de', 't0001', [(u"bitte schalte das Licht in der Werkstatt ein", u"", check_light, ['light_on', 'aiHLWorkshop'])])
 
-    k.dte.ts('en', 't0004', [(u"computer, please switch off the lights in the dining room", u"", check_light, ['light_off', 'aiHLDiningRoom'])])
-    k.dte.ts('de', 't0005', [(u"Computer, schalte mal das Licht im Schlafzimmer aus", u"", check_light, ['light_off', 'aiHLBedroom'])])
+    k.dte.ts('en', 't0004', [(u"please switch off the lights in the dining room", u"", check_light, ['light_off', 'aiHLDiningRoom'])])
+    k.dte.ts('de', 't0005', [(u"schalte mal das Licht im Schlafzimmer aus", u"", check_light, ['light_off', 'aiHLBedroom'])])
 
     k.dte.dt('en', u"(please|) lights {light_actions:LABEL} {home_locations:PL} {home_locations:LABEL}",
                    turn_lights, ['home_locations_0_start', 'home_locations_0_end', 'light_actions_0_action'])
     k.dte.dt('de', u"(bitte|) Licht {light_actions:LABEL} {home_locations:PL} {home_locations:LABEL}",
                    turn_lights, ['home_locations_0_start', 'home_locations_0_end', 'light_actions_0_action'])
 
-    k.dte.ts('en', 't0002', [(u"computer, lights on in the kitchen", u"", check_light, ['light_on', 'aiHLKitchen'])])
+    k.dte.ts('en', 't0002', [(u"lights on in the kitchen", u"", check_light, ['light_on', 'aiHLKitchen'])])
     k.dte.ts('de', 't0003', [(u"Licht an im Keller", u"", check_light, ['light_on', 'aiHLBasement'])])
 
     k.dte.dt('en', u"(please|) lights {light_actions:LABEL} (please|)",
@@ -89,10 +92,10 @@ def get_data(k):
     k.dte.dt('de', u"(bitte|) (schalte|mach) (bitte|) (mal|) das Licht {light_actions:LABEL} (bitte|)",
                    turn_lights, [-1, -1, 'light_actions_0_action'])
 
-    k.dte.ts('en', 't0006', [(u"computer lights on in the living room", u"", check_light, ['light_on', 'aiHLLivingRoom']),
+    k.dte.ts('en', 't0006', [(u"lights on in the living room", u"", check_light, ['light_on', 'aiHLLivingRoom']),
                              (u"switch on the lights in the attic", u"", check_light, ['light_on', 'aiHLAttic']),
                              (u"please turn off the lights", u"", check_light, ['light_off', 'aiHLAttic'])])
-    k.dte.ts('de', 't0007', [(u"computer licht an im Wohnzimmer", u"", check_light, ['light_on', 'aiHLLivingRoom']),
+    k.dte.ts('de', 't0007', [(u"licht an im Wohnzimmer", u"", check_light, ['light_on', 'aiHLLivingRoom']),
                              (u"schalte das licht auf dem dachboden ein", u"", check_light, ['light_on', 'aiHLAttic']),
                              (u"bitte schalte das licht aus", u"", check_light, ['light_off', 'aiHLAttic'])])
 
